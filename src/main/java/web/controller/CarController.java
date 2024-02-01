c74b9367d3621ffa706cd3677f3d761a35d72815
tda78 <tda78@mail.ru>
2024-02-01 22:57:59 +0700

    corrected version1.0

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,7 +1,6 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -12,22 +11,18 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping("/cars")
 public class CarController {
-    @Autowired
+
     CarService service;
+
+    @Autowired
+    public CarController(CarService service) {
+        this.service = service;
+    }
 
     @GetMapping("")
 
-    public String getCount(HttpServletRequest request, ModelMap model) {
-        int count;
-        if (request.getParameter("count") == null) {
-            count = 5;
-        } else {
+    public String getCount(@RequestParam(defaultValue = "5") int count, ModelMap model) {
 
-            count = Integer.parseInt(request.getParameter("count"));
-            if (count == 0 || count > 5) {
-                count = 5;
-            }
-        }
         model.addAttribute("cars", service.getCars(count));
         return "showcars";
     }
